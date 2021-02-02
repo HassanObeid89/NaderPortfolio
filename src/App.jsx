70d179d95178d0f4152fc6d81dc8e319d5addabb
HassanObeid89 <61018141+HassanObeid89@users.noreply.github.com>
2021-02-02 21:46:56 +0100

    Added NavBar component and installed react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
-
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './css/App.css';
 
 // Import pages
 
 import HomePage from './components/home/HomePage';
-
-
+import Navbar from './components/navbar/Navbar';
+import About from './components/aboutMe/About';
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Router>
+        <Navbar />
+
+        <Switch>
+          <Route path='/' exact>
+            <HomePage />
+          </Route>
+          <Route path='/About-me' exact>
+            <About />
+          </Route>
+          
+          </Switch>
+
+      </Router>
+
     </div>
   );
 }
