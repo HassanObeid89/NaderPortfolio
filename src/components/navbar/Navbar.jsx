6f0e946dee6de21e98c49ec52606c2d7d94693dc
HassanObeid89 <61018141+HassanObeid89@users.noreply.github.com>
2021-02-04 21:30:42 +0100

    Styled the navBar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../../css/navBar.css';
+
 function Navbar() {
     return (
-        <div>
+        <div className = 'header'>
             <ul>
                 <li>
                     <Link to='/'>
                         Home
                     </Link>
                 </li>
+            
                 <li>
                     <Link to='/About-me'>
-                        About Me
+                        About
                     </Link>
                 </li>
             </ul>
